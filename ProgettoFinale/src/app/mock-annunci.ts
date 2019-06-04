f46d387be81c215ddb405983908093c226397808
MolodduwaGabriel <molodduwa.quiroz.gabriel.andres@iisgalvanimi.edu.it>
2019-06-04 23:35:33 +0000

    rifatto per malfunzionamento

---
+++ ProgettoFinale/src/app/mock-annunci.ts
@@ -0,0 +1,18 @@
+import {Annuncio} from './Annuncio';
+export const DefAnnunci =
+[
+    {
+        username: "Gabriel",
+        data: "14/06/2019",
+        titolo: "Iphone 8",
+        descrizione: "Telefono",
+        risposte:
+        [
+            {
+                username: "admin",
+                testo: "Salve,sono interessato all'articolo ma vorrei avere più dettegli ",
+                data: "17/06/2019",
+            }
+        ]
+    }
+]
